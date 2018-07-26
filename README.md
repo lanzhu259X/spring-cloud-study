d5c7a6bf1048eb87ad9a5905233882b063965143
lanzhu259X <liliang259X@163.com>
2018-07-26 18:02:12 +0800

    spring cloud demo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring Cloud Micro Service Demo
+
+> 测试 Spring Cloud 搭建微服务。各种实例部署到Docker上，使用Docker来验证信息
+
+## Eureka 注册中心
+
+## Config 配置中心
+
+## service-user 服务提供者Demo
+
+## consumer-user 服务消费者Demo
+
+## api-gateway API网关
+
+## Hystrix && Hystrix Dashboard 熔断器
---
+++ service-user/service-demo-a/src/main/java/com/lanzhu/service/demo/controller/DemoController.java
@@ -0,0 +1,38 @@
+package com.lanzhu.service.demo.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/servicedemo/a")
+@RefreshScope
+public class DemoController {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
+    @Value("${info}")
+    private Object info;
+
+    @Value("${info.title}")
+    private String title;
+
+    @GetMapping(value = "/title", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getTitle() {
+        log.info("title:{}", title);
+        return this.title;
+    }
+
+    @GetMapping(value = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getInfo() {
+        log.info("info:{}", JSON.toJSONString(info));
+        return JSON.toJSONString(info);
+    }
+
+}
